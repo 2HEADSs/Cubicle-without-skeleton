2e97e3750c5c074a9012afead8af4d4478f0fbdf
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-10-03 13:16:11 +0300

    Add dynamic content to attach accessory page

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { getById } = require('../services/cubeService')
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const cube = await getById(req.params.id);
     res.render('details', {
         cube
@@ -9,7 +9,7 @@ router.get('/:id', async(req, res) => {
 });
 
 
-router.get('/:id/attach', async(req, res) => {
+router.get('/:id/attach', async (req, res) => {
     console.log(req.params.id);
     const cube = await getById(req.params.id);
     res.render('attachAccessory', {
